2cdac8c0f8d88f52a2348f2fef716ae7e0bf30ed
Murilo S Dias <msdias@unb.br>
2020-07-03 09:26:38 -0300

    organizando os arquivos basicos

--- analises.R
+++ analises.R
@@ -1,6 +1,8 @@
 library(vegan)
 source("R/Clean_data.R")
 
-dir.create("output/figures/")
-dir.create("R/")
-dir.create("doc/")
+# dir.create("R/")
+# dir.create("data/")
+# dir.create("output/data/")
+# dir.create("output/figures/")
+# dir.create("doc/")
